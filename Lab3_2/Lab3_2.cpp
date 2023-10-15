2dce4d7b174ca9a61ac31708db2e0a98b6e46008
Никита <@DESKTOP-0N74OFC>
2023-10-15 18:24:59 +0700

    Добавьте файлы проекта.

---
+++ Lab3_2/Lab3_2.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	double a,b,c;
+	cin >> a >> b >> c;
+	double ab = 0, ac = 0, bc = 0;
+	if (a < b and b < c) {
+		ab = b - a;
+		ac = c - a;
+		bc = c - b;
+		cout << ab << ' ' << ac << ' ' << bc << ' ';
+	}
+	double max;
+	max = ab;
+	if (max < ac) {
+		max = ac;
+	}
+	if (max < bc) {
+		max = bc;
+	}
+	cout << max;
+}
+
+	
